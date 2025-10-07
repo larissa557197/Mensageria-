27d02120346ea4402f4b5824fbd958da94eddb04
larissa557197 <larissampmuniz@gmail.com>
2025-10-07 11:44:47 -0300

    first commit

---
+++ src/test/java/br/com/fiap/mensageria/MensageriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mensageria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensageriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
